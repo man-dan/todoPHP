57b06893411e87c9b2ff5cc3414576cc8a223b0d
man-dan <man-dan@mail.ru>
2016-10-16 13:19:02 +0300

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is project which keep all your tasks!!!
+You can test it on this page : https://agile-brook-70154.herokuapp.com/
--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 
 <div id="ms"><form method="POST" id="formx" action="javascript:void(null);">
-	<input type="text" name="listname" id="listname" required placeholder="Enter name of list"  maxlength="51"/>
+	<input type="text" name="listname" id="listname" required placeholder="Enter name of list"  maxlength="50"/>
 	<input type="submit" id="save" value="Save name of list"/>
 </form></div>
 
